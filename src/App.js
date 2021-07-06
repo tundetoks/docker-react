84d14e4240e9f8ecb06c747faf5cae014b08a3f8
Olatunde Tokun <olatundetokun@MAC-OTT-TEC-SECUR1.local>
2021-07-05 22:13:41 -0400

    another change to App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed in the feature branch
+          I was changed in the feature branch. But im not seeing anything. Sadface
         </p>
         <a
           className="App-link"
