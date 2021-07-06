629dde56230b998c12b90ebba95e8d7f1aac95a5
Olatunde Tokun <olatundetokun@MAC-OTT-TEC-SECUR1.local>
2021-07-05 21:56:06 -0400

    modify text on feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world!
+          I was changed in the feature branch
         </p>
         <a
           className="App-link"
